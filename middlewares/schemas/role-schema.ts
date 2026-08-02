1555ce9a30dab256b53c3c8c4a74a5e4a9b0fab4
Amjad Adi <1230800@student.birzeit.edu>
2026-08-02 11:02:44 +0300

    update role routes and added postman collection

--- middlewares/schemas/role-schema.ts
+++ middlewares/schemas/role-schema.ts
@@ -1,8 +1,17 @@
 import { z} from "zod"
 import {Status} from "../../models/enums/model-status"
-export let roleValidation=z.object({
+export let getRoleValidation=z.object({
     id:z.bigint(),
     uuid:z.uuid(),
-    name:z.string().nonempty().max(128).nullish(),
+    name:z.string().nonempty().max(128),
     status:z.nativeEnum(Status)
-});
+}).strict();
+
+export let createRoleValidation=z.object({
+    name:z.string().nonempty().max(128),
+}).strict();
+
+export let updateRoleValidation=z.object({
+    name:z.string().nonempty().max(128),
+    status:z.nativeEnum(Status)
+}).strict();
--- repositories/role.repository.ts
+++ repositories/role.repository.ts
@@ -3,11 +3,12 @@ import {pool} from "../databases/postgre-connection"
 import {COLUMN_NAME, COLUMN_STATUS, COLUMN_UUID, TABLE_NAME} from "../databases/contract/role.contract"
 import {QueryResult} from "pg";
 import {NotFoundError} from "../errors/not-found-error";
+import {Status} from "../models/enums/model-status";
 
 export async function findAll():Promise<QueryResult<any>>{
     try{
         return await pool.query(
-            `SELECT *
+            `SELECT ${COLUMN_UUID},${COLUMN_NAME}, ${COLUMN_STATUS}
              FROM ${TABLE_NAME}`)
     } catch (e) {
         console.error(e)
@@ -18,7 +19,7 @@ export async function findAll():Promise<QueryResult<any>>{
 export async function findById(uuid:string):Promise<QueryResult<any>>{
     try {
         return await pool.query(
-            `SELECT *
+            `SELECT ${COLUMN_UUID},${COLUMN_NAME}, ${COLUMN_STATUS}
              FROM ${TABLE_NAME}
              WHERE ${COLUMN_UUID} = $1`, [uuid])
     } catch (e) {
@@ -30,8 +31,10 @@ export async function findById(uuid:string):Promise<QueryResult<any>>{
 export async function create(role: CreateRole):Promise<QueryResult<any>> {
     try{
         return await pool.query(
-        `INSERT INTO ${TABLE_NAME}
-                        VALUES ($1)`, [role.name]);
+        `INSERT INTO ${TABLE_NAME}(${COLUMN_NAME},${COLUMN_STATUS})
+                        VALUES ($1,$2)
+                        RETURNING ${COLUMN_UUID},${COLUMN_NAME},${COLUMN_STATUS}`,
+                        [role.name,Status.ACTIVE]);
     } catch (e) {
         console.error(e)
         throw new Error()
@@ -44,7 +47,9 @@ export async function update(role: UpdateRole, uuid:string):Promise<QueryResult<
             `UPDATE ${TABLE_NAME}
              SET ${COLUMN_NAME}=$1,
                  ${COLUMN_STATUS}=$2
-             WHERE ${COLUMN_UUID} = $3`, [role.name, role.status, uuid]);
+             WHERE ${COLUMN_UUID} = $3
+             RETURNING ${COLUMN_UUID},${COLUMN_NAME},${COLUMN_STATUS}`,
+            [role.name, role.status, uuid]);
     }catch (e) {
     console.error(e)
         throw new Error()
