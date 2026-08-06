e821487460bb5107b9bf13c4e9d7124f5792fab6
Amjad Adi <1230800@student.birzeit.edu>
2026-08-06 17:21:49 +0300

    Implemented Registration APIs and Login simulation

--- permissions/roles-permissions.ts
+++ permissions/roles-permissions.ts
@@ -1,3 +1,10 @@
-import {CREATE_USER_PERMISSION, GET_USERS_PERMISSION} from "./permissions"
+import {
+    READ_USERS,
+    READ_CURRENT_USER,
+    WRITE_USER_AS_ADMIN,
+    CREATE_USER,
+    WRITE_CURRENT_USER
+} from "./permissions"
 export const rolesPermissions=new Map<String, string[]>;
-rolesPermissions.set("SUPER ADMIN", [GET_USERS_PERMISSION,CREATE_USER_PERMISSION])
+rolesPermissions.set("SUPER ADMIN", [WRITE_USER_AS_ADMIN,CREATE_USER,READ_CURRENT_USER,READ_USERS])
+rolesPermissions.set("CUSTOMER", [READ_CURRENT_USER,WRITE_CURRENT_USER])
--- routes/main-router.route.ts
+++ routes/main-router.route.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import {userRouter} from "./user.route";
 import {organizationRouter} from "./organization.route";
-import {authenticationRouter} from "./authentication.route";
+
 export let mainRouter=express.Router()
 mainRouter.use("/users",userRouter)
 mainRouter.use("/organizations",organizationRouter)
-mainRouter.use("/auth", authenticationRouter)
