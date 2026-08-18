56fa705503e1cea1dd6440ee89370dfa93839820
Amjad Adi <1230800@student.birzeit.edu>
2026-08-18 17:26:24 +0300

    Implemented Time Block API

--- databases/drizzle-schemas/enums.ts
+++ databases/drizzle-schemas/enums.ts
@@ -1,5 +1,10 @@
 import {pgEnum} from "drizzle-orm/pg-core";
+import {ActivationStatus} from "../../models/enums/activation-status";
+import {Role} from "../../models/enums/roles";
+import {RoomOccupancyStatus} from "../../models/enums/room-occupancy-status";
+import {TimeBlockStatus} from "../../models/enums/time-block-status";
 
-export const activationStatusEnum=pgEnum("status",["ACTIVE","INACTIVE"])
-export const roleEnum=pgEnum("role",["SUPER_ADMIN","OWNER","MANAGER","CRM","WORKER","CUSTOMER"])
-export const occupancyStatusEnum=pgEnum("occupancy_status",["OCCUPIED","AVAILABLE"])
+export const activationStatusEnum=pgEnum("status",Object.values(ActivationStatus) as [ActivationStatus, ...ActivationStatus[]]);
+export const roleEnum=pgEnum("role",Object.values(Role) as [Role, ...Role[]])
+export const occupancyStatusEnum=pgEnum("occupancy_status",Object.values(RoomOccupancyStatus) as [RoomOccupancyStatus, ...RoomOccupancyStatus[]])
+export const timeBlockStatusEnum=pgEnum("request_status",Object.values(TimeBlockStatus) as [TimeBlockStatus, ...TimeBlockStatus[]])
