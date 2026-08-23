9b20d4756265b6a267db8458ed8a318310a9d42e
Amjad Adi <1230800@student.birzeit.edu>
2026-08-23 07:36:38 +0300

    Commit Appointment API's Implementation

--- databases/drizzle-schemas/room.db.ts
+++ databases/drizzle-schemas/room.db.ts
@@ -2,19 +2,21 @@ import {drizzleConnection} from "../drizzle-connection";
 import {pgTable, bigint, primaryKey, uuid, varchar, date, timestamp} from "drizzle-orm/pg-core";
 import {
     COLUMN_CREATED_AT_UTC,
-    COLUMN_UPDATED_AT_UTC,COLUMN_ORGANIZATION_ID,
-    TABLE_NAME
+    COLUMN_UPDATED_AT_UTC, COLUMN_ORGANIZATION_ID,
+    TABLE_NAME, COLUMN_USER_ID
 } from "../contracts/room.contract";
 import {activationStatusEnum, occupancyStatusEnum} from "./enums";
 import {organizationTable} from "./organizations.db";
 import {ActivationStatus} from "../../models/enums/activation-status";
 import {RoomOccupancyStatus} from "../../models/enums/room-occupancy-status";
+import {usersTable} from "./users.db";
 
 export const roomTable= pgTable(TABLE_NAME,{
     id:bigint({mode:"number"}).primaryKey().generatedAlwaysAsIdentity(),
     uuid:uuid().defaultRandom().unique().notNull(),
     name:varchar({length:256}).notNull(),
     description:varchar({length:4096}),
+    userId:bigint(COLUMN_USER_ID,{mode:"number"}).notNull().references(()=>usersTable.id),
     organizationId:bigint(COLUMN_ORGANIZATION_ID,{mode:"number"}).notNull().references(()=>organizationTable.id),
     createdAtUTC:timestamp(COLUMN_CREATED_AT_UTC,{withTimezone:true}).notNull().defaultNow(),
     updatedAtUTC:timestamp(COLUMN_UPDATED_AT_UTC,{withTimezone:true}).notNull().defaultNow(),
---
+++ middlewares/zod-schemas/appointment.schema.ts
@@ -0,0 +1,76 @@
+import {z} from "zod"
+import {PaymentMethod} from "../../models/enums/payment-method";
+import {DEFAULT_COLOUR} from "../../models/appointment.model";
+import {AppointmentStatus} from "../../models/enums/appointment-status";
+import {ActivationStatus} from "../../models/enums/activation-status";
+import {querySchema} from "./query.schema";
+import {
+    SORT_BY_NAME,
+    SORT_BY_PAID_AT_UTC,
+    SORT_BY_SCHEDULED_END_AT_UTC, SORT_BY_SCHEDULED_START_AT_UTC
+} from "../../databases/contracts/appointment.contract";
+import {AppointmentTimeType} from "../../models/enums/appointment-time-type";
+import {PaymentStatus} from "../../models/enums/payment-status";
+
+export const createAppointmentSchema=z.object({
+    userNote:z.string().trim().max(4096).nonempty().optional(),
+    scheduledStartTimeUTC:z.iso.datetime({offset:true}),
+    scheduledEndTimeUTC:z.iso.datetime({offset:true}),
+    userColour:z.string().regex(/^#[a-f0-9]{6}$/i,
+        {message: 'Invalid color format. Must be a 7-character hex code (e.g., #RRGGBB).'}).default(DEFAULT_COLOUR),
+    //checks user colour against hex format, i flag used for insensitivity
+    paymentMethod:z.enum(PaymentMethod),
+}).strict()
+
+export const updateAppointmentSchemaByUser=z.object({
+    userNote:z.string().trim().nonempty().max(4096).optional(),
+    userColour:z.string().regex(/^#[a-f0-9]{6}$/i,
+        {message: 'Invalid color format. Must be a 7-character hex code (e.g., #RRGGBB).'}).default(DEFAULT_COLOUR),
+}).strict()
+
+export const updateAppointmentSchemaByOrganization=z.object({
+    organizationNote:z.string().trim().nonempty().max(4096).optional(),
+    organizationColour:z.string().regex(/^#[a-f0-9]{6}$/i,
+        {message: 'Invalid color format. Must be a 7-character hex code (e.g., #RRGGBB).'}).default(DEFAULT_COLOUR),
+}).strict()
+
+export const confirmAppointmentSchema=z.object({
+    name:z.string().trim().nonempty().max(256),
+    organizationColour:z.string().regex(/^#[a-f0-9]{6}$/i,
+        {message: 'Invalid color format. Must be a 7-character hex code (e.g., #RRGGBB).'}).default(DEFAULT_COLOUR),
+    organizationNote:z.string().trim().nonempty().max(4096).optional()
+}).strict()
+
+export const rejectAppointmentSchemaBy=z.object({
+    rejectionReason:z.string().trim().nonempty().max(4096),
+}).strict()
+
+export const updateAppointmentSchemaStatus=z.object({
+    appointmentStatus:z.enum(AppointmentStatus).refine((status)=>(status!=AppointmentStatus.PENDING&&status!=AppointmentStatus.REJECTED)),
+}).strict()
+
+export const payAppointmentSchema=z.object({
+    paymentMethod: z.enum(PaymentMethod),
+}).strict()
+
+
+export const appointmentFilterSchema = z.object({
+    appointmentStatus:z.enum(ActivationStatus).optional(),
+    appointmentDate:z.iso.date().optional(),
+    appointmentTimeType:z.enum(AppointmentTimeType).optional(),
+    fromDate:z.iso.date().optional(),
+    toDate:z.iso.date().optional(),
+    userUuid:z.uuid().optional(),
+    approvalUserUuid:z.uuid().optional(),
+    employeeUuid:z.uuid().optional(),
+    serviceUuid:z.uuid().optional(),
+    roomUuid:z.uuid().optional(),
+    paymentMethod:z.enum(PaymentMethod),
+    paymentStatus:z.enum(PaymentStatus),
+}).strict();
+
+export const queryAppointmentSchema = querySchema.extend({
+    search: z.string().trim().nonempty().max(256).optional(),
+    filter: appointmentFilterSchema.optional(),
+    sortBy: z.enum([SORT_BY_NAME, SORT_BY_SCHEDULED_START_AT_UTC,SORT_BY_SCHEDULED_END_AT_UTC,SORT_BY_PAID_AT_UTC]).optional(),
+}).strict();
